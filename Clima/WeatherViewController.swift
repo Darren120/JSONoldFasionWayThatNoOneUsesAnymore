1a06d6de5e642e0daa6735e81bc7424ef40d45a3
Darren Zou <zoudarrenn7@gmail.com>
2017-11-02 17:56:08 -0400

    ok

--- Clima/WeatherViewController.swift
+++ Clima/WeatherViewController.swift
@@ -41,23 +41,40 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate, change
     
     //MARK: - Networking
     /***************************************************************/
-    
+//    let lat = param["lat"]!
+//    let lon = param["lon"]!
+//    let appid = param["appid"]!
+//    let url = URL(string: "http://api.openweathermap.org/data/2.5/weather?lat=\(lat)&lon=\(lon)&appid=\(appid)")
     //Write the getWeatherData method here:
     
     func getWeatherData(param: [String: String]) {
-        let lat = param["lat"]!
-        let lon = param["lon"]!
-        let appid = param["appid"]!
-        let url = URL(string: "http://api.openweathermap.org/data/2.5/weather?lat=\(lat)&lon=\(lon)&appid=\(appid)")
+
+        var url: URL!
+        if let lat = param["lat"], let lon = param["lon"], let appid = param["appid"] {
+            url = URL(string: "http://api.openweathermap.org/data/2.5/weather?lat=\(lat)&lon=\(lon)&appid=\(appid)")!
+        } else if let cityName = param["q"], let appid = param["appid"] {
+            url = URL(string: "http://api.openweathermap.org/data/2.5/weather?q=\(cityName)&appid=\(appid)")
+        } else {
+            url = URL(string: "http://api.openweathermap.org/data/2.5/weather")
+        }
         print("ok")
-        let task = URLSession.shared.dataTask(with: url!) {
+        let task = URLSession.shared.dataTask(with: url) {
             (data, response, error) in
             do {
-                let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as! NSDictionary
+                let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as AnyObject
+                print(json)
                 if let temp = json["main"] as? [String: AnyObject]{
-                    let temperature = temp["temp"] as! NSNumber
+                    let temperature = temp["temp"] as! Double
                     self.weatherDetailModel.temperature = Int(temperature)
                     self.updateUIWithWeatherData()
+                }
+                if let name = json["name"] as? String {
+                    self.weatherDetailModel.city = name
+                    
+                }
+                if let id = json["sys"] as? [String: AnyObject] {
+                    let id = id["id"] as! Int
+                    self.weatherDetailModel.condition = id
                 }
               
             } catch {
@@ -79,9 +96,9 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate, change
     
     
     //Write the updateWeatherData method here:
-    func updateWeatherData(json: AnyObject) {
-        temperatureLabel.text = String(weatherDetailModel.temperature)
-    }
+//    func updateWeatherData(json: AnyObject) {
+//        temperatureLabel.text = String(weatherDetailModel.temperature)
+//    }
 
     
     
@@ -93,7 +110,9 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate, change
     //Write the updateUIWithWeatherData method here:
     func updateUIWithWeatherData() {
         DispatchQueue.main.async {
-            self.temperatureLabel.text = String(self.weatherDetailModel.temperature)
+            self.weatherIcon.image = UIImage(named: self.weatherDetailModel.updateWeatherIcon(condition: self.weatherDetailModel.condition))
+            self.temperatureLabel.text = String(self.weatherDetailModel.temperature - Int(273.15))
+            self.cityLabel.text = self.weatherDetailModel.city
         }
         
     }
